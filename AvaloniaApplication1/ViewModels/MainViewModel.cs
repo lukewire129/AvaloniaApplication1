71d13bdaff13518f35ddb3c4a57342d8fa90d043
lukewire <xlwpdldhk@gmail.com>
2023-07-25 11:58:58 +0900

    초기 커밋.

---
+++ AvaloniaApplication1/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.ObjectModel;
+
+namespace AvaloniaApplication1.ViewModels;
+
+public partial class MainViewModel : ViewModelBase
+{
+    private string Greeting => "Welcome to Avalonia!";
+    [ObservableProperty]
+    private string allowInput;
+    [ObservableProperty]
+    private ObservableCollection<string> items;
+
+
+    public MainViewModel()
+    {
+        Items = new ObservableCollection<string>();
+    }
+    [RelayCommand]
+    private void Count()
+    {
+        if(Items == null)
+            Items = new ObservableCollection<string>();
+
+        Items.Add (Greeting);
+    }
+}
